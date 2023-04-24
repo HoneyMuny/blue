0800abcfd9908f4a8f4034aa653bfc0e5b29973a
Diana Acosta <diceacmu1503@gmail.com>
2023-04-24 13:10:40 -0600

    Initial commit

---
+++ lib/principalPage.dart
@@ -0,0 +1,99 @@
+import 'dart:async';
+
+import 'package:blue/SelectBondedDevicePage.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
+
+class PrincipalPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new MyPrincipalPage();
+  }
+}
+
+class MyPrincipalPage extends State<PrincipalPage>{
+  BluetoothState _bluetoothState = BluetoothState.UNKNOWN;
+  String _address = "...";
+  String _name = "...";
+
+  Timer? _discoverableTimeoutTimer;
+  int _discoverableTimeoutSecondsLeft = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    FlutterBluetoothSerial.instance.state.then((state) {
+
+      setState(() {
+        _bluetoothState = state;
+      });
+
+    });
+
+    Future.doWhile(() async {
+      if((await FlutterBluetoothSerial.instance.isEnabled)?? false) {
+        return false;
+      }
+      await Future.delayed(Duration(milliseconds: 0xDD));
+      return true;
+    }).then((_) {
+      FlutterBluetoothSerial.instance.address.then((address) {
+        setState(() {
+          _address = address!;
+        });
+      });
+    });
+
+    FlutterBluetoothSerial.instance.name.then((name) {
+      setState(() {
+        _name = name!;
+      });
+    });
+
+    FlutterBluetoothSerial.instance.onStateChanged().listen((BluetoothState state) {
+      setState(() {
+        _bluetoothState = state;
+        _discoverableTimeoutTimer = null;
+        _discoverableTimeoutSecondsLeft = 0;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    FlutterBluetoothSerial.instance.setPairingRequestHandler(null);
+    _discoverableTimeoutTimer?.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar (
+        title: Text("Conexiòn Bluetooth"),
+      ),
+      body: Container (
+        child: ListView(
+          children: <Widget>[
+            ListTile(
+              title: ElevatedButton(
+                onPressed: () async{
+                  final BluetoothDevice? selectedDevice =
+                      await Navigator.of(context).push(
+                          MaterialPageRoute(
+                              builder: (context){
+                                return SelectBondedDevicePage(checkAvailability: false);
+                              }
+                              ),
+                      );
+                },
+                child: Text("Ver dispositivos disponibles :)"),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+}
